ad19bc670b0d0d02906d8c8c68bbbcc2d8ecf7f1
Sven Aas <sven.aas@gmail.com>
2014-11-06 12:19:42 -0500

    Add usage, execution modes, and some Twitter code. Refactor.

--- md.rb
+++ md.rb
@@ -4,10 +4,18 @@ require "nokogiri"
 
 ATOM_URL = 'http://mefideleted.blogspot.com/feeds/posts/default'
 
-def reasons 
+@client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = ENV["CONSUMER_KEY"]
+  config.consumer_secret     = ENV["CONSUMER_SECRET"]
+  config.access_token        = ENV["OAUTH_TOKEN"]
+  config.access_token_secret = ENV["OAUTH_TOKEN_SECRET"]
+end
+
+# Get the latest deletion reasons from the MeFi Deleted blog's feed
+def deletion_reasons 
 	feed = RSS::Parser.parse(ATOM_URL, false)
 
-	hash = feed.entries.each_with_object({}) do |entry, hash| 		
+	reasons = feed.entries.each_with_object({}) do |entry, hash| 		
 		content = Nokogiri::HTML(entry.content.content)
 		content.css('span.reason').text =~ /(.*) (-- .*$)/
 		reason = $1
@@ -16,4 +24,33 @@ def reasons
 	end
 end
 
-reasons.keys.sort.each {|k| puts "#{k}: #{reasons[k]}"}
+# Get the 20 newest followers, ordered from newest to oldest
+def followers
+  @client.followers.take(20)
+end
+
+# Print list of most recent deletion resons
+def list	
+	reasons = deletion_reasons
+	reasons.keys.sort.each {|k| puts "#{k}: #{reasons[k]}"}
+end
+
+# Print usage instructions
+def usage 
+  puts "Usage:"
+  puts "  ea.rb run        - normal execution      (may post to Twitter)"
+  puts "  ea.rb list       - list deletion reasons"
+  puts "  ea.rb followers  - list 20 newest followers"
+end
+
+if ARGV.size != 1
+  usage
+elsif ARGV[0] == 'run'
+  run
+elsif ARGV[0] == 'list'
+  list
+elsif ARGV[0] == 'followers'
+  followers.each {|f| puts "@#{f.username}"}
+else 
+  usage
+end
